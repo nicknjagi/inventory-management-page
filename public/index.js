0d98859c6b0183a3947fdb1af5c8e1142a35fc3a
Nicholas Njagi <njaginicholas@gmail.com>
2023-10-10 10:56:21 +0300

    show filter options on filter-btn click

--- public/index.js
+++ public/index.js
@@ -1,8 +1,24 @@
 const searchForm = document.getElementById('search-form')
-
-
-
+const categoryBtn = document.getElementById('category-btn')
+const quantityBtn = document.getElementById('quantity-btn')
+const priceBtn = document.getElementById('price-btn')
+const categoryOptions = document.getElementById('category-options')
+const quantityOptions = document.getElementById('quantity-options')
+const priceOptions = document.getElementById('price-options')
 
 searchForm.addEventListener('submit', (e)=> {
   e.preventDefault()
 })
+
+categoryBtn.addEventListener('click',() => {
+  categoryOptions.classList.toggle('show-options')
+})
+
+quantityBtn.addEventListener('click',() => {
+  quantityOptions.classList.toggle('show-options')
+})
+
+priceBtn.addEventListener('click',() => {
+  priceOptions.classList.toggle('show-options')
+})
+
